50acce51e11ab4f0f1c7b7c4f651b0044ca5fca3
Gabriel Ribeiro <gabrielribeiro@gabriels-macbook-pro.local>
2016-04-25 13:15:15 -0400

    Initial Commit

---
+++ Final Project (UCL Stadiums)Tests/Final_Project__UCL_Stadiums_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Final_Project__UCL_Stadiums_Tests.swift
+//  Final Project (UCL Stadiums)Tests
+//
+//  Created by Gabriel Ribeiro on 4/25/16.
+//  Copyright © 2016 SHP. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Project__UCL_Stadiums_
+
+class Final_Project__UCL_Stadiums_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
